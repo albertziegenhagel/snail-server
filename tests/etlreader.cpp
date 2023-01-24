fb33e5d09070884a60483781cb5c92c6fbee0673
Albert <albert.ziegenhagel@outlook.com>
2023-01-24 17:54:48 +0100

    Remove old unused files

--- tests/etlreader.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <gtest/gtest.h>
-
-#include <snail/etl/etlreader.hpp>
-
-TEST(EtlReader, Init)
-{
-    snail::etl_reader reader(R"(C:\Users\aziegenhagel\source\snail-support\tests\data\hidden_sc.user_aux.etl)");
-}
