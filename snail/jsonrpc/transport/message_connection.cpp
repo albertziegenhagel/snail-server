3b39195dfa22e3e17618c4abe8fa66a1216f9678
Albert <albert.ziegenhagel@outlook.com>
2023-02-12 11:59:11 +0100

    [jsonrpc] remove debug output

--- snail/jsonrpc/transport/message_connection.cpp
+++ snail/jsonrpc/transport/message_connection.cpp
@@ -5,8 +5,6 @@
 #include <snail/jsonrpc/transport/message_reader.hpp>
 #include <snail/jsonrpc/transport/message_writer.hpp>
 
-#include <iostream>
-
 using namespace snail::jsonrpc;
 
 message_connection::message_connection(std::unique_ptr<message_reader> reader,
@@ -23,14 +21,9 @@ void message_connection::serve(message_handler& handler)
     {
         auto message = reader_->read();
 
-        std::cout << "<-- " << message << "\n";
-        std::cout.flush();
-
         const auto result = handler.handle(message);
         if(result)
         {
-            std::cout << "--> " << *result << "\n";
-            std::cout.flush();
             writer_->write(*result);
         }
     }
