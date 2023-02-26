7237c198c325649f139dacb84f74ba34a8e0ea01
Albert <albert.ziegenhagel@outlook.com>
2023-02-26 14:24:27 +0100

    Remove SNAIL_WARNINGS_AS_ERRORS
    Since CMake 3.24 there  is `CMAKE_COMPILE_WARNING_AS_ERROR`

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -1,8 +1,6 @@
 
 add_library(compile_options INTERFACE)
 
-option(SNAIL_WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
-
 if(MSVC)
   if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
     string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
@@ -12,22 +10,12 @@ if(MSVC)
     "/W4"
     "/permissive-"
   )
-  if(SNAIL_WARNINGS_AS_ERRORS)
-    target_compile_options(compile_options INTERFACE
-        "/WX"
-    )
-  endif()
 elseif(CMAKE_COMPILER_IS_GNUCC)
   target_compile_options(compile_options INTERFACE
     "-Wall"
     "-Wextra"
     "-pedantic"
   )
-  if(SNAIL_WARNINGS_AS_ERRORS)
-    target_compile_options(compile_options INTERFACE
-        "-Werror"
-    )
-  endif()
 endif()
 
 target_compile_features(compile_options INTERFACE cxx_std_23)
