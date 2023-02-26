9e645b3edab0ffb38d698042fe33a3905a8ecc10
Albert <albert.ziegenhagel@outlook.com>
2023-02-26 12:49:48 +0100

    Add utility to create a random file name

--- tests/common/utility.cpp
+++ tests/common/utility.cpp
@@ -30,3 +30,20 @@ TEST(Trim, Both)
     EXPECT_EQ(trim("   a b\tc\nd   "), "a b\tc\nd"sv);
     EXPECT_EQ(trim("  \n\t a b\tc\nd   "), "a b\tc\nd"sv);
 }
+
+TEST(RandomFilename, Create)
+{
+    const char allowed_characters[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    const std::size_t desired_length = 20;
+
+    const auto name = make_random_filename(desired_length);
+    EXPECT_EQ(name.size(), desired_length);
+    EXPECT_EQ(name.find_first_not_of(allowed_characters), std::string_view::npos) << "name: " << name;
+}
+
+TEST(RandomFilename, Empty)
+{
+    const auto name = make_random_filename(0);
+    EXPECT_EQ(name.size(), 0);
+}
