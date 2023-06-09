1a26e6870864ec40b799680391625b1531283c58
Albert <albert.ziegenhagel@outlook.com>
2023-06-09 16:18:16 +0200

    Add compiler options for source encoding

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -1,6 +1,7 @@
 
 add_library(compile_options INTERFACE)
 
+# Warning flags
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
   if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
     string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
@@ -15,6 +16,18 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_FRONTEND_VARIA
     "-Wall"
     "-Wextra"
     "-pedantic"
+  )
+endif()
+
+# Source encoding
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  target_compile_options(compile_options INTERFACE
+    "/utf-8"
+  )
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  target_compile_options(compile_options INTERFACE
+    "-finput-charset=UTF-8"
+    # No need to set `-fexec-charset` since the default is UTF-8 (at least in recent versions)
   )
 endif()
 
