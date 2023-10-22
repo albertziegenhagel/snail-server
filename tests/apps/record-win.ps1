59fffc300fbd0a0ecff7a9c3532895e48fc36efc
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2023-10-22 17:49:17 +0200

    Debug output

--- tests/apps/record-win.ps1
+++ tests/apps/record-win.ps1
@@ -5,5 +5,5 @@ param(
     [String]$OutputName
 )
 
-Start-Process $VsDiagnosticsPath start, $SessionId, "/launch:${AppPath}", "/loadAgent:4EA90761-2248-496C-B854-3C0399A591A4;DiagnosticsHub.CpuAgent.dll" -Wait
-Start-Process $VsDiagnosticsPath stop, $SessionId, /output:${OutputName} -Wait
+Start-Process $VsDiagnosticsPath start, $SessionId, "/launch:${AppPath}", "/loadAgent:4EA90761-2248-496C-B854-3C0399A591A4;DiagnosticsHub.CpuAgent.dll" -Wait -NoNewWindow
+Start-Process $VsDiagnosticsPath stop, $SessionId, /output:${OutputName} -Wait -NoNewWindow
