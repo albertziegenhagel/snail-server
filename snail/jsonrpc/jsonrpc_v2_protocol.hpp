41e2e02a2449820cea67677a0a99276003f6d4f8
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2024-10-12 19:36:25 +0200

    Add const to some member function that do not change the state of this

--- snail/jsonrpc/jsonrpc_v2_protocol.hpp
+++ snail/jsonrpc/jsonrpc_v2_protocol.hpp
@@ -7,11 +7,11 @@ namespace snail::jsonrpc {
 class v2_protocol : public protocol
 {
 public:
-    [[nodiscard]] virtual request load_request(std::string_view content) override;
+    [[nodiscard]] virtual request load_request(std::string_view content) const override;
 
-    [[nodiscard]] virtual std::string dump_response(const jsonrpc::response& response) override;
+    [[nodiscard]] virtual std::string dump_response(const jsonrpc::response& response) const override;
 
-    [[nodiscard]] virtual std::string dump_error(const rpc_error& error, const nlohmann::json* id) override;
+    [[nodiscard]] virtual std::string dump_error(const rpc_error& error, const nlohmann::json* id) const override;
 };
 
 } // namespace snail::jsonrpc
