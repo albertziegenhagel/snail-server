106056e8825e33b7d3398739afd5d74daa74e606
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2024-10-12 19:35:47 +0200

    Add thread pool utility

---
+++ snail/common/thread_pool.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <vector>
+
+namespace snail::common {
+
+class thread_pool
+{
+public:
+    using task_type = std::function<void()>;
+
+    explicit thread_pool(std::size_t max_size);
+    ~thread_pool();
+
+    void submit(task_type task);
+
+    void stop();
+
+private:
+    std::size_t              max_size_;
+    std::vector<std::thread> workers_;
+
+    std::queue<task_type> task_queue_;
+
+    std::mutex              mutex_;
+    std::condition_variable condition_variable_;
+
+    bool stopped_;
+
+    void spawn_thread();
+};
+
+} // namespace snail::common
